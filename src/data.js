bd1c9e731194add1a5136cc16092b643d787f532
Rezab Ud Dawla <rezabuddawlatuhin@gmail.com>
2023-11-03 19:49:42 +0600

    drag and drop added

--- src/data.js
+++ src/data.js
@@ -1,46 +1,46 @@
 export const data = [
     {
-        id: "1",
+        id: 1,
         name: "src/assets/images/image-1.webp"
     },
     {
-        id: "2",
+        id: 2,
         name: "src/assets/images/image-2.webp"
     },
     {
-        id: "3",
+        id: 3,
         name: "src/assets/images/image-3.webp"
     },
     {
-        id: "4",
+        id: 4,
         name: "src/assets/images/image-4.webp"
     },
     {
-        id: "5",
+        id: 5,
         name: "src/assets/images/image-5.webp"
     },
     {
-        id: "6",
+        id: 6,
         name: "src/assets/images/image-6.webp"
     },
     {
-        id: "7",
+        id: 7,
         name: "src/assets/images/image-7.webp"
     },
     {
-        id: "8",
+        id: 8,
         name: "src/assets/images/image-8.webp"
     },
     {
-        id: "9",
+        id: 9,
         name: "src/assets/images/image-9.webp"
     },
     {
-        id: "10",
+        id: 10,
         name: "src/assets/images/image-10.jpeg"
     },
     {
-        id: "11",
+        id: 11,
         name: "src/assets/images/image-11.jpeg"
     },
 ];
